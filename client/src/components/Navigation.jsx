517c8f81baae5736fa482d2d662028e2c65a631b
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-10-09 17:01:40 +0100

    added nav bar roles

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -12,151 +12,130 @@ import { LinkContainer } from "react-router-bootstrap";
 import bringLogo from "../assets/bring2.svg";
 
 const Navigation = props => {
-  return (
-    <Navbar fixed="top" collapseOnSelect expand="lg" bg="light">
-      <Navbar.Brand href="#home">
-        <img
-          src={bringLogo}
-          width="40"
-          height="40"
-          className="d-inline-block align-top"
-          alt="React Bootstrap logo"
-        />
-      </Navbar.Brand>
-      {(!props.user && (
-        <Fragment>
-          <Link className="btn" to="/sign-in">
-            Sign In
-          </Link>
-          <Link className="btn" to="/">
-            Sign Up
-          </Link>
-        </Fragment>
-      )) || (
-        <Fragment>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <LinkContainer to="/profile">
-                <Nav.Link href="#features">Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/donationOrg">
-                <Nav.Link>Request Donation</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/signup">
-                <Nav.Link>Sugest Donation</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/signup">
-                <Nav.Link>Volunteer News</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/signup">
-                <Nav.Link>Edit Profile</Nav.Link>
-              </LinkContainer>
-              <Form onSubmit={props.signOut}>
-                <Button type="submit">Sign Out</Button>
-              </Form>
-            </Nav>
-          </Navbar.Collapse>
-        </Fragment>
-      )}
-    </Navbar>
-  );
+  if (props.user) {
+    if (props.user.role === "User") {
+      //---------------------------VOLUNTEER/DONOR!! FOR STYLING---------------------------------
+      return (
+        <div>
+          <Navbar
+            fixed="top"
+            collapseOnSelect
+            expand="lg"
+            bg="dark"
+            variant="dark"
+          >
+            <Navbar.Brand href="#home">
+              {" "}
+              <img
+                src={bringLogo}
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <LinkContainer to="/profile">
+                  <Nav.Link href="#features">Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/donation">
+                  <Nav.Link href="#features">Browse Donations</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/profile">
+                  <Nav.Link href="#features">
+                    Donations to be picked up
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/profile">
+                  <Nav.Link href="#features">My picked Donations</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/profile">
+                  <Nav.Link href="#features">My User profile</Nav.Link>
+                </LinkContainer>
+                <Form onSubmit={props.signOut}>
+                  <Button type="submit">Sign Out</Button>
+                </Form>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      );
+      //---------------------------ORGANIZATION !! FOR STYLING---------------------------------
+    } else if (props.user.role === "Organization") {
+      return (
+        <div>
+          <Navbar
+            fixed="top"
+            collapseOnSelect
+            expand="lg"
+            bg="dark"
+            variant="dark"
+          >
+            <Navbar.Brand href="#home">
+              {" "}
+              <img
+                src={bringLogo}
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <LinkContainer to="/profile">
+                  <Nav.Link href="#features">Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/donationOrg">
+                  <Nav.Link href="#features">Request Donation</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/donationOrg">
+                  <Nav.Link href="#features">Sugest Donation</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/donationOrg">
+                  <Nav.Link href="#features">Volunteer News</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/profile">
+                  <Nav.Link href="#features">My Organization profile</Nav.Link>
+                </LinkContainer>
+                <Form onSubmit={props.signOut}>
+                  <Button type="submit">Sign Out</Button>
+                </Form>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      );
+    }
+    //------------------------------------------------------------------
+  } else {
+    return (
+      <Navbar fixed="top" collapseOnSelect expand="lg" bg="light">
+        <Navbar.Brand href="#home">
+          <img
+            src={bringLogo}
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        {(!props.user && (
+          <Fragment>
+            <Link className="btn" to="/sign-in">
+              Sign In
+            </Link>
+            <Link className="btn" to="/">
+              Sign Up
+            </Link>
+          </Fragment>
+        )) || <div>{this.ShowPageVolunteerOrOrganization()}</div>}
+      </Navbar>
+    );
+  }
 };
 
 export default withRouter(Navigation);
-
-{
-  /*  <Navbar
-          fixed="top"
-          collapseOnSelect
-          expand="lg"
-          bg="dark"
-          variant="dark"
-        >
-          <Navbar.Brand href="#home">Inverte</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <LinkContainer to="/">
-                <Nav.Link href="#features">Home</Nav.Link>
-              </LinkContainer>
-              <Nav.Link href="#pricing">Missao</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <LinkContainer to="/signup">
-                <Nav.Link>Sign up</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/signin">
-                <Nav.Link>Sign in</Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar> */
-}
-
-{
-  /* <Navbar bg="light" fixed="top" collapseOnSelect expand="lg">
-      <Navbar.Brand href="/donation">
-        <img
-          src={bringLogo}
-          width="40"
-          height="40"
-          className="d-inline-block align-top"
-          alt="React Bootstrap logo"
-        />
-      </Navbar.Brand>
-      {(!props.user && (
-        <Fragment>
-          <Link className="btn" to="/sign-in">
-            Sign In
-          </Link>
-          <Link className="btn" to="/">
-            Sign Up
-          </Link>
-        </Fragment>
-      )) || (
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <LinkContainer to="/">
-              <Nav.Link href="#features">Home</Nav.Link>
-            </LinkContainer>
-            <Nav.Link href="#pricing">Missao</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <LinkContainer to="/signup">
-              <Nav.Link>Sign up</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/signin">
-              <Nav.Link>Sign in</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      )}
-    </Navbar>
-  );
-}; */
-}
